69767630d9da5c77e3c92921507503aec1615677
bklopcic <benjaminklopcic@gmail.com>
2017-11-14 11:40:18 -0500

    Polyjuice is my shit

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "*hacker voice* i'm in" << std::endl;
-    std::cout <<"Draco Malfoy is fake news" << std::endl;
+    std::cout <<"Malfoy is fake news" << std::endl;
     return 0;
 }
