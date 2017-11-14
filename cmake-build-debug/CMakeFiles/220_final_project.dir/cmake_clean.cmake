f65369d2c1233af97a66c6a3ed80f8853295ea58
bklopcic <benjaminklopcic@gmail.com>
2017-11-14 11:20:33 -0500

    Harry Potter stuff, bois

---
+++ cmake-build-debug/CMakeFiles/220_final_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/220_final_project.dir/main.cpp.o"
+  "220_final_project.pdb"
+  "220_final_project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/220_final_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/220_final_project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 220_final_project.
+# This may be replaced when dependencies are built.
